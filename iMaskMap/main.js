d4940f0be1aa59d01de492c907579dcea08da599
fysh711426 <fysh711426@hotmail.com>
2020-02-11 11:34:52 +0800

    update iMaskMap

--- iMaskMap/main.js
+++ iMaskMap/main.js
@@ -1,5 +1,8 @@
-﻿var map = L.map('map', {
-    center: [22.604799, 120.2976256],
+﻿var latitude = getParameterByName('latitude') || 24.2113218;
+var longitude = getParameterByName('longitude') || 120.6942998;
+
+var map = L.map('map', {
+    center: [latitude, longitude],
     zoom: 12
 });
 
@@ -7,9 +10,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+var redIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+L.marker([latitude, longitude], { icon: redIcon }).addTo(map);
+
 var markers = new L.MarkerClusterGroup().addTo(map);
 
-var greenIcon = new L.Icon({
+var imageIcon = new L.Icon({
     iconUrl: 'https://ithelp.ithome.com.tw/upload/images/20200210/20106865an2gNAyvHQ.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [40, 40],
@@ -28,9 +41,9 @@ xhr.onload = function () {
             data[i].geometry.coordinates[1],
             data[i].geometry.coordinates[0]
         ], {
-            icon: greenIcon
+            icon: imageIcon
         }).bindPopup(data[i].properties.name);
         markers.addLayer(mark);
     }
     map.addLayer(markers);
-}
+};
