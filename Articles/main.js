ef8f69b3169fe44e690f6dbc3697f54498551585
fysh711426 <fysh711426@hotmail.com>
2020-03-21 23:12:57 +0800

    update Articles

--- Articles/main.js
+++ Articles/main.js
@@ -1,6 +1,12 @@
-window.onload = function() {
+var fontFace = new FontFace(
+    'huninn',
+    'url(jf-openhuninn-1.0.ttf) format("woff2")');
+fontFace.display = 'auto';
+document.fonts.add(fontFace);
+fontFace.load();
+fontFace.load().then(function () {
     document.body.classList.add('loaded');
-};
+});
 
 function toggle(e) {
     var children = e.nextElementSibling;
