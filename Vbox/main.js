80c20ea29aa0a2cb7ae91aea55118dcf80beda16
fysh711426 <fysh711426@hotmail.com>
2021-08-27 21:15:19 +0800

    add Vbox

---
+++ Vbox/main.js
@@ -0,0 +1,5 @@
+﻿window.addEventListener('scroll', function(){
+    var scrollPositionY = window.pageYOffset;
+    var header = document.querySelector('.header');
+    header.style.transform = 'translateY(' + scrollPositionY * 0.25 + 'px)';
+});
