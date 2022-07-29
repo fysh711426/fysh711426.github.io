4801fe2a1b66e16c1f3d6ddeff78b3f372940664
fysh711426 <fysh711426@hotmail.com>
2022-07-29 22:57:09 +0800

    add vtuber

---
+++ vtuber/index.js
@@ -0,0 +1,50 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        // apiBase: 'https://cdn.statically.io/gh/fysh711426/VtuberData/master/api/',
+        apiBase: 'https://raw.githubusercontent.com/fysh711426/VtuberData/master/api/',
+        menu: '',
+        submenu: '',
+        day: '',
+        showDay: false,
+        showRankVar: false,
+        isLoaded: false,
+        datas: []
+    },
+    methods: {
+        init: async function () {
+            this.menu = getParameter('m') || 'popular';
+            this.submenu = getParameter('s') || 'tw';
+            if (this.menu === 'popular' || this.menu === 'singing') {
+                this.day = getParameter('d') || '7';
+                this.showDay = true;
+            }
+            this.showRankVar = true;
+            // if (this.menu === 'popular' || this.menu === 'subscribe') {
+            //     this.showRankVar = true;
+            // }
+            $m.selectMenu(this.menu);
+            $m.selectSubMenu(this.submenu);
+            await this.getData();
+            this.isLoaded = true;
+            setTimeout(function () {
+                $m.tooltip('.tooltip');
+            }, 1);
+        },
+        getApi: function () {
+            return this.apiBase +
+                this.menu + '_' +
+                this.submenu + (this.showDay ? '_day' + this.day : '') + '.json';
+        },
+        getData: async function () {
+            var api = this.getApi();
+            if (api) {
+                var response = await axios.get(api);
+                this.datas = response.data;
+            }
+        }
+    },
+    mounted() {
+        this.init();
+    }
+});
---
+++ vtuber/title.js
@@ -0,0 +1,16 @@
+(function () {
+    var menu = getParameter('m') || 'popular';
+    document.title = 'Vtuber - ' + getTitle();
+    function getTitle() {
+        if (menu === 'popular')
+            return '熱度排行';
+        if (menu === 'subscribe')
+            return '訂閱排行';
+        if (menu === 'singing')
+            return '歌回影片';
+        if (menu === 'growing')
+            return '本周成長';
+        if (menu === 'newcomer')
+            return '本月新人';
+    }
+})();
