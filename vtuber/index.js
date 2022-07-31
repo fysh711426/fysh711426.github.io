1d47fb05806a5e3373bfa20d7797464b911392e5
fysh711426 <fysh711426@gmail.com>
2022-07-31 16:21:45 +0000

    auto-commit

--- vtuber/index.js
+++ vtuber/index.js
@@ -9,6 +9,7 @@ var vm = new Vue({
         showDay: false,
         showRankVar: false,
         isLoaded: false,
+        errorImage: null,
         datas: []
     },
     methods: {
@@ -26,7 +27,9 @@ var vm = new Vue({
             $m.selectMenu(this.menu);
             $m.selectSubMenu(this.submenu);
             $m.gotop('.gotop');
-            await this.getData();
+            this.errorImage = await this.loadedErrorImage(
+                'https://cdn.statically.io/gh/fysh711426/fysh711426.github.io/master/cdn/mini-form-ui/images/default-head-176x176.png');
+            this.datas = await this.getData();
             this.isLoaded = true;
             setTimeout(function () {
                 $m.tooltip('.tooltip');
@@ -41,8 +44,29 @@ var vm = new Vue({
             var api = this.getApi();
             if (api) {
                 var response = await axios.get(api);
-                this.datas = response.data;
+                return response.data;
             }
+        },
+        imgError: function (e) {
+            if (this.errorImage) {
+                e.target.src = this.errorImage;
+            }
+        },
+        loadedErrorImage: function (url) {
+            return new Promise(function (resolve) {
+                var img = new Image();
+                img.onload = function () {
+                    if (this.complete === true) {
+                        resolve(url);
+                        img = null;
+                    }
+                }
+                img.onerror = function () {
+                    resolve(null);
+                    img = null;
+                }
+                img.src = url;
+            });
         }
     },
     mounted() {
