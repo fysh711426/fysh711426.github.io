f79a82c1395c110019885c2b358762b7bb34e35f
fysh711426 <fysh711426@gmail.com>
2022-08-03 05:42:48 +0000

    auto-commit

--- mini-form-ui/scroll.js
+++ mini-form-ui/scroll.js
@@ -6,26 +6,37 @@ var onScrollEnd = (function () {
         return Math.max(window.pageYOffset || 0,
             document.documentElement.scrollTop || document.body.scrollTop);
     }
-    // function getScrollHeight() {
-    //     return document.documentElement.scrollHeight || document.body.scrollHeight;
-    // }
-    function getClientHeight() {
-        return document.documentElement.clientHeight || document.body.clientHeight;
-    }
-    function getDocHeight() {
-        var D = document;
+    function getScrollHeight() {
         return Math.max(
-            D.body.scrollHeight, D.documentElement.scrollHeight,
-            D.body.offsetHeight, D.documentElement.offsetHeight,
-            D.body.clientHeight, D.documentElement.clientHeight
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
         );
     }
-    return function (callback, distance) {
+    function getClientHeight() {
+        return Math.max(window.innerHeight || 0,
+            document.documentElement.clientHeight || document.body.clientHeight);
+    }
+    return function (callback, distance, delay) {
         distance = distance || 0;
-        document.addEventListener('scroll', function () {
-            if (getScrollTop() + getClientHeight() + distance >= getDocHeight()) {
+        onScroll(function () {
+            if (getScrollTop() + getClientHeight() + distance >= getScrollHeight()) {
                 callback.call(this);
             }
-        });
+        }, delay);
     }
 })();
+
+var onScroll = function (callback, delay) {
+    var scheduled = false;
+    delay = delay || 100;
+    document.addEventListener('scroll', function () {
+        if (!scheduled) {
+            scheduled = true;
+            setTimeout(function () {
+                callback.call(this);
+                scheduled = false;
+            }, delay);
+        }
+    });
+}
